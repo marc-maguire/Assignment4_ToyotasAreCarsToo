f281b8ddb188c3a0fa00bdb34aa560821e358011
Marc Maguire <maguire.marc@gmail.com>
2017-02-03 18:18:00 +0800

    initial commit

---
+++ ToyotasAreCarsToo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ToyotasAreCarsToo
+//
+//  Created by Marc Maguire on 2017-02-03.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
